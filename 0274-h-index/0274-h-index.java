7bb41732d4d8586a162e2a960c9020fb1b0663cf
Adarsh S <64910179+ada-rsh-s@users.noreply.github.com>
2025-01-03 20:35:30 +0530

    Time: 0 ms (100%), Space: 42 MB (10.06%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,19 +1,19 @@
-import java.util.Arrays;
-
 class Solution {
     public int hIndex(int[] citations) {
-        Arrays.sort(citations);
-        int length = citations.length;
-        int hindex = 0;
+        int papers = citations.length;
+        int[] citationBuckets = new int[papers + 1];
 
-        for (int i = 0; i < length; i++) {
-            int h = length - i;
-            if (citations[i] >= h) {
-                hindex = h;
-                break;
-            }
+        for (int citation : citations) {
+            citationBuckets[Math.min(citation, papers)]++;
         }
 
-        return hindex;
+        int cumulativePapers = 0;
+        for (int hIndex = papers; hIndex >= 0; hIndex--) {
+            cumulativePapers += citationBuckets[hIndex];
+            if (cumulativePapers >= hIndex) {
+                return hIndex;
+            }
+        }
+        return 0;        
     }
 }
