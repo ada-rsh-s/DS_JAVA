84ada035bf23748ff7af4c873c958b4864d01f21
Adarsh S <64910179+ada-rsh-s@users.noreply.github.com>
2025-03-02 17:43:39 +0530

    Time: 2475 ms (5.01%), Space: 46.9 MB (5.18%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -1,62 +1,30 @@
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 class Solution {
     public List<Integer> findSubstring(String s, String[] words) {
         int sLength = s.length();
-        int wordsCount = words.length;
-        int wordLength = words[0].length();
-        int totalWordLength = wordLength * wordsCount;
+        int wordsLength = words.length;
+        int singleWordLength = words[0].length();
+        int totalWordLength = singleWordLength * wordsLength;
 
+        Map<String, Integer> wordFreq = new HashMap<>();
         List<Integer> startIndices = new ArrayList<>();
 
-        if (sLength < totalWordLength) return startIndices;
-
-        // Build the word frequency map
-        Map<String, Integer> wordFreq = new HashMap<>();
         for (String word : words) {
             wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
         }
 
-        // Slide the window
-        for (int i = 0; i < wordLength; i++) {
-            int left = i, right = i;
+        for (int i = 0; i <= sLength - totalWordLength; i++) {
             Map<String, Integer> currWordFreq = new HashMap<>();
-            int count = 0;
 
-            while (right + wordLength <= sLength) {
-                // Get the next word
-                String word = s.substring(right, right + wordLength);
-                right += wordLength;
+            String currString = s.substring(i, i + totalWordLength);
 
-                // Check if it's a valid word
-                if (wordFreq.containsKey(word)) {
-                    currWordFreq.put(word, currWordFreq.getOrDefault(word, 0) + 1);
-                    count++;
-
-                    // Shrink window if word count exceeds allowed frequency
-                    while (currWordFreq.get(word) > wordFreq.get(word)) {
-                        String leftWord = s.substring(left, left + wordLength);
-                        currWordFreq.put(leftWord, currWordFreq.get(leftWord) - 1);
-                        left += wordLength;
-                        count--;
-                    }
-
-                    // Check for a valid concatenation
-                    if (count == wordsCount) {
-                        startIndices.add(left);
-                    }
-                } else {
-                    // Reset the window if invalid word found
-                    currWordFreq.clear();
-                    count = 0;
-                    left = right;
-                }
+            for (int j = 0; j < totalWordLength; j += singleWordLength) {
+                String currWord = currString.substring(j, j + singleWordLength);
+                currWordFreq.put(currWord, currWordFreq.getOrDefault(currWord, 0) + 1);
             }
+            if (currWordFreq.equals(wordFreq))
+                startIndices.add(i);
         }
-
         return startIndices;
     }
+
 }
