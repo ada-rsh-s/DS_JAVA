f1e74ac389af8a3965174b72e3963060c2e1a4ae
Adarsh S <64910179+ada-rsh-s@users.noreply.github.com>
2025-03-03 07:18:40 +0530

    Time: 11 ms (97.48%), Space: 45.4 MB (74.08%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -3,9 +3,12 @@ class Solution {
         int sLength = s.length();
         int wordsLength = words.length;
         int singleWordLength = words[0].length();
+        int totalWordLength = singleWordLength * wordsLength;
 
         Map<String, Integer> wordFreq = new HashMap<>();
         List<Integer> startIndices = new ArrayList<>();
+
+        if (sLength < totalWordLength) return startIndices;
 
         for (String word : words) {
             wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
@@ -15,22 +18,26 @@ class Solution {
         for (int i = 0; i < singleWordLength; i++) {
             Map<String, Integer> currWordFreq = new HashMap<>();
             int left = i, right = i;
+            int validWordCount=0;
             while (right <= sLength - singleWordLength) {
                 String currWord = s.substring(right, right + singleWordLength);
                 right+= singleWordLength;
                 if (wordFreq.containsKey(currWord)) {
                     currWordFreq.put(currWord, currWordFreq.getOrDefault(currWord, 0) + 1);
+                    validWordCount++;
 
                     while (currWordFreq.get(currWord) > wordFreq.get(currWord)) {
                         String remWord = s.substring(left, left + singleWordLength);
                          currWordFreq.put(remWord, currWordFreq.getOrDefault(remWord, 0) - 1);
+                         validWordCount--;
                         left += singleWordLength;
                     }
-                    if (currWordFreq.equals(wordFreq))
+                    if (validWordCount==wordsLength)
                         startIndices.add(left);
                 } else {
                     currWordFreq.clear();
                     left=right;
+                    validWordCount=0;
 
                 }
             }
