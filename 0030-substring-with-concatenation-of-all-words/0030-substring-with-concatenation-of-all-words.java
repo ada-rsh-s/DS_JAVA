5459c82594d85abee79bf00c48b70257a49efc50
Adarsh S <64910179+ada-rsh-s@users.noreply.github.com>
2025-03-03 07:08:27 +0530

    Time: 15 ms (44.83%), Space: 45.7 MB (51.26%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.java
@@ -3,53 +3,41 @@ class Solution {
         int sLength = s.length();
         int wordsLength = words.length;
         int singleWordLength = words[0].length();
-        int totalWordLength = singleWordLength * wordsLength;
 
         Map<String, Integer> wordFreq = new HashMap<>();
         List<Integer> startIndices = new ArrayList<>();
 
-        // Build the frequency map for words
         for (String word : words) {
             wordFreq.put(word, wordFreq.getOrDefault(word, 0) + 1);
+
         }
 
-        // Iterate over every possible starting position
         for (int i = 0; i < singleWordLength; i++) {
-            int left = i, right = i;
             Map<String, Integer> currWordFreq = new HashMap<>();
-            int count = 0;
+            int left = i, right = i;
+            while (right <= sLength - singleWordLength) {
+                String currWord = s.substring(right, right + singleWordLength);
+                right+= singleWordLength;
+                if (wordFreq.containsKey(currWord)) {
+                    currWordFreq.put(currWord, currWordFreq.getOrDefault(currWord, 0) + 1);
 
-            // Slide the window over the string
-            while (right + singleWordLength <= sLength) {
-                String word = s.substring(right, right + singleWordLength);
-                right += singleWordLength;
-
-                // If the word is valid, add it to the current window
-                if (wordFreq.containsKey(word)) {
-                    currWordFreq.put(word, currWordFreq.getOrDefault(word, 0) + 1);
-                    count++;
-
-                    // Shrink the window if the frequency of the word exceeds the allowed frequency
-                    while (currWordFreq.get(word) > wordFreq.get(word)) {
-                        String leftWord = s.substring(left, left + singleWordLength);
-                        currWordFreq.put(leftWord, currWordFreq.get(leftWord) - 1);
+                    while (currWordFreq.get(currWord) > wordFreq.get(currWord)) {
+                        String remWord = s.substring(left, left + singleWordLength);
+                         currWordFreq.put(remWord, currWordFreq.getOrDefault(remWord, 0) - 1);
                         left += singleWordLength;
-                        count--;
                     }
-
-                    // If all words are found, record the starting index
-                    if (count == wordsLength) {
+                    if (currWordFreq.equals(wordFreq))
                         startIndices.add(left);
-                    }
                 } else {
-                    // Reset the window if an invalid word is found
                     currWordFreq.clear();
-                    count = 0;
-                    left = right;
+                    left=right;
+
                 }
             }
         }
-
         return startIndices;
+
+    
     }
+
 }
