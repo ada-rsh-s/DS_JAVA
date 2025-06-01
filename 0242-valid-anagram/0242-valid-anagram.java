07145e59d1c493cb8df5dd1d1e61c99479dea111
Adarsh S <64910179+ada-rsh-s@users.noreply.github.com>
2025-06-01 21:06:46 +0530

    Time: 2 ms (98%), Space: 43.3 MB (71.86%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,30 +1,24 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        HashMap<Character, Integer> anagram = new HashMap<>();
-
-        if (s.length() != t.length())
-            return false;
-
-        for (int i = 0; i < s.length(); i++) {
-            char sChar = s.charAt(i);
-            anagram.put(sChar, anagram.getOrDefault(sChar, 0) + 1);
+        int[] count = new int[26];
+        
+        // Count the frequency of characters in string s
+        for (char x : s.toCharArray()) {
+            count[x - 'a']++;
         }
-
-        for (int i = 0; i < t.length(); i++) {
-            char tChar = t.charAt(i);
-            if (anagram.containsKey(tChar)) {
-                anagram.put(tChar, anagram.get(tChar) - 1);
-                if (anagram.get(tChar) == 0)
-                    anagram.remove(tChar);
-            } else {
+        
+        // Decrement the frequency of characters in string t
+        for (char x : t.toCharArray()) {
+            count[x - 'a']--;
+        }
+        
+        // Check if any character has non-zero frequency
+        for (int val : count) {
+            if (val != 0) {
                 return false;
             }
         }
-
-        if (anagram.isEmpty())
-            return true;
-        else
-            return false;
+        
+        return true;
     }
-
 }
